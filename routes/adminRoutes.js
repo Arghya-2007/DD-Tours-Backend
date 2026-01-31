47500eeb985eb2e7c08886bae02190741e252828
Arghya Pal <arghya.github@gmail.com>
2026-01-31 10:25:39 +0530

    Make Auth

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { adminLogin } = require("../controllers/adminController");
+const { adminLogin } = require("../controllers/adminAuthController");
 
 // POST /api/admin/login
 router.post("/login", adminLogin);
